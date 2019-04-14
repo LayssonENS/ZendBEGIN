bc0df5bd9574d1b5a663a5d60306668fc4e38fe8
Laysson Enderson <laysson_enderson@hotmail.com>
2019-04-14 11:31:49 -0300

    pacotes

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,6 +11,23 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\Paginator',
+    'Zend\Mail',
+    'Zend\ServiceManager\Di',
+    'Zend\Mvc\Plugin\FilePrg',
+    'Zend\Mvc\Plugin\FlashMessenger',
+    'Zend\Mvc\Plugin\Identity',
+    'Zend\Mvc\Plugin\Prg',
+    'Zend\Session',
+    'Zend\Mvc\I18n',
+    'Zend\Mvc\Console',
+    'Zend\Form',
+    'Zend\InputFilter',
+    'Zend\Filter',
+    'Zend\Hydrator',
+    'Zend\I18n',
+    'Zend\Db',
+    'Zend\Cache',
     'Zend\Router',
     'Zend\Validator',
     'Application',
